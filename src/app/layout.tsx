cffbe81cdd5b2af6c9dff429babd6bd45018f800
gary <gary@dtx.trade>
2025-03-18 11:52:24 +0800

    fix: 🐛 google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "@/styles/globals.css";
 import { MainNav } from "@/components/main-nav";
-import Head from "next/head";
 const geistSans = Geist({
   weight: ["400", "500", "700"],
   subsets: ["latin"],
@@ -22,9 +21,9 @@ export default function RootLayout({
  
   return (
     <html lang="en">
-      <Head>
-        <meta name="google-adsense-account" content="ca-pub-3198470578962995" />
-      </Head>
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3198470578962995" crossorigin="anonymous"></script>
+      </head>
       <body className={geistSans.className}>
         <MainNav />
       <div className="container mx-auto py-20 mt-10">
